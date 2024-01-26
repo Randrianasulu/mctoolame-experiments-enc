de8567e2f3eb19fe6ffb846e1b815541cd71136e
Andrew Randrianasulu <randrianasulu@gmail.com>
2024-01-26 18:17:56 +0300

    Add new fft code from  twolame

--- fft.h
+++ fft.h
@@ -2,3 +2,4 @@
 /* The following functions are in the file "subs.c" */
 
 extern void        fft(float *x_real, float *x_imag, float *energy, float *phi);
+void psycho_1_fft (FLOAT * x_real, FLOAT * energy, int N);
